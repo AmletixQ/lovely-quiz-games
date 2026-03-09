5beb42c9a5996da93b5d03c61e1aae224c928815
AmletixQ <nazarovamridin308@gmail.com>
2026-03-09 19:37:12 +0300

    Последние приготовления

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import HomePage from "./pages/home.page.tsx";
 import "./index.css";
+
 import RootLayout from "./layouts/root.layout.tsx";
-import GamesPage from "./pages/games.page.tsx";
-import GamePage from "./pages/game.page.tsx";
+import {
+  AnniversaryPage,
+  CongratulationsPage,
+  GamePage,
+  GamesPage,
+  HomePage,
+} from "./pages/index.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -21,16 +25,8 @@ createRoot(document.getElementById("root")!).render(
             <Route path=":id" element={<GamePage />} />
           </Route>
 
-          <Route path="anniversary">
-            <Route
-              index
-              element={
-                <div className="text-primary flex h-screen w-screen items-center justify-center text-5xl font-bold">
-                  Soon...
-                </div>
-              }
-            />
-          </Route>
+          <Route path="anniversary" element={<AnniversaryPage />} />
+          <Route path="congratulations" element={<CongratulationsPage />} />
         </Route>
       </Routes>
     </Router>
